b5108d096c069596c06e52705108ade2d1d4c1e8
abdulsagheer <73389096+abdulsagheer@users.noreply.github.com>
2021-03-08 10:10:34 +0530

    ok

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://Abdul:sagheer@cluster0.thgmf.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Abdul:sagheer@cluster0.thgmf.mongodb.net/todolistDB", {useNewUrlParser: true,useUnifiedTopology:true});
 
 const itemsSchema = {
   name: String
